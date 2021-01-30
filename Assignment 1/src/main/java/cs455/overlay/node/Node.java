99ee9f6e8b540ddc7ae93e615c91931e99c68d00
Jean-Marc Ruffalo-Burgat <me@jeanmarc.us>
2021-01-30 15:22:47 -0700

    fixed bugs in error handling

--- Assignment 1/src/main/java/cs455/overlay/node/Node.java
+++ Assignment 1/src/main/java/cs455/overlay/node/Node.java
@@ -1,10 +1,14 @@
 package cs455.overlay.node;
 
-import java.io;
-import java.net;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Random;
 
-public class Node(){
+public class Node {
     private Socket socket;
     private ServerSocket serverSocket;
     private DataOutputStream dout;
@@ -15,39 +19,52 @@ public class Node(){
     public long totSent = 0;
     private int port;
 
-    public Node(int port){
+    public Node(int port) {
         this.port = port;
     }
 
-    public reciever(String serverAddress, int port){
-        this.socket = new Socket(serverAddress,port);
+    public void reciever(String serverAddress, int port) throws IOException {
+        try {
+            this.socket = new Socket(serverAddress, port);
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         this.din = new DataInputStream(this.socket.getInputStream());
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             this.recieveTracker++;
             this.totRecived += din.readInt();
         }
     }
 
-    public closeReciever(){
+    public void closeReciever() throws IOException {
         this.din.close();
         this.socket.close();
     }
 
-    public sender(){
-        this.serverSocket = new ServerSocket(this.port, 5);
+    public void sender() throws IOException{ 
+        try {
+            this.serverSocket = new ServerSocket(this.port, 5);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         this.socket = this.serverSocket.accept();
         this.dout = new DataOutputStream(this.socket.getOutputStream());
         Random rand = new Random();
         int randInt = 0;
-        for(int i = 0; i < 5; i++){
-            randInt = Random.nextInt();
+        for (int i = 0; i < 5; i++) {
+            randInt = rand.nextInt();
             this.dout.writeInt(randInt);
             this.sendTracker++;
             this.totSent += randInt;
         }
     }
 
-    public closeSender(){
+    public void closeSender() throws IOException {
         this.dout.close();
         this.serverSocket.close();
     }

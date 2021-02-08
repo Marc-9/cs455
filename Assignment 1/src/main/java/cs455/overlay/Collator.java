4fa8093698a304e478a59a68fc1d72f23a099240
Jean-Marc Ruffalo-Burgat <me@jeanmarc.us>
2021-02-08 00:49:54 -0700

    working demo, however it is quite slow

--- Assignment 1/src/main/java/cs455/overlay/Collator.java
+++ Assignment 1/src/main/java/cs455/overlay/Collator.java
@@ -27,7 +27,13 @@ public class Collator {
     public int totConnections;
     public int connections = 0;
     public int totRounds = 0;
-    public ArrayList<Node> nodes = new ArrayList<Node>();
+    public ArrayList<Socket> nodes = new ArrayList<Socket>();
+    public ArrayList<String[]> socketInfo = new ArrayList<String[]>();
+    public ArrayList<DataInputStream> inputStreams = new ArrayList<DataInputStream>();
+    public ArrayList<DataOutputStream> outputStreams = new ArrayList<DataOutputStream>();
+    public int totSent = 0;
+    public long recieveSummation = 0;
+    public long sendSummation = 0;
 
     public Collator(String hostName, int portNumber, int totConnections) {
         this.hostName = hostName;
@@ -43,46 +49,92 @@ public class Collator {
     public void listenForRegistrations() throws IOException {
         while (this.connections < this.totConnections) {
             this.socket = this.serverSocket.accept();
-
-            this.din = new DataInputStream(this.socket.getInputStream());
-            int nameLength = din.readInt();
-            byte[] identifierBytes = new byte[nameLength];
-            din.readFully(identifierBytes);
             this.connections++;
-            //this.nodes.add(new String(identifierBytes));
-            System.out.println(new String(identifierBytes));
-
-            /*DataInputStream dis = new DataInputStream(s.getInputStream()); 
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-            int nameLength = din.readInt();
+            DataInputStream din2 = new DataInputStream(this.socket.getInputStream());
+            DataOutputStream dout9 = new DataOutputStream(this.socket.getOutputStream());
+            inputStreams.add(din2);
+            outputStreams.add(dout9);
+            int nameLength = din2.readInt();
             byte[] identifierBytes = new byte[nameLength];
-            din.readFully(identifierBytes);
-            String nodeHostname = new String(identifierBytes);
-            ArrayList<String> alteredServerList = newServerList(nodeHostName, this.serverList);
+            din2.readFully(identifierBytes);
+            int socketPort = din2.readInt();
+            this.nodes.add(this.socket);
 
-
-            Thread t = new ClientHandler(s, dis, dos, new String(identifierBytes), alteredServerList); 
-            nodes.push(t);*/
-            this.socket.close();
-
-            this.connections++;
-
+            String socketName = new String(identifierBytes);
+            String[] temp = {socketName, String.valueOf(socketPort)};
+            this.socketInfo.add(temp);
+            System.out.println(socketName);
         }
 
-       
 
     }
 
 
 
-    public void startMessagePassing() throws IOException{ 
-        for(int j = 0; j < 5000; j++){
-            for(int i = 0; i < this.connections; i++){
-                int recieverIndex = this.randomNum(this.connections, i);
-                this.nodes.get(i).sender();
-                this.nodes.get(recieverIndex).reciever(this.nodes.get(i).hostName, 8952);
+    public void startMessagePassing() throws IOException{
+        for(int i = 0; i < this.connections; i++){
+            System.out.println("Starting with " + this.socketInfo.get(i)[0]);
+            for(int j = 0; j < 1000; j++){
+                //DataOutputStream dout2 = new DataOutputStream(this.nodes.get(i).getOutputStream());
+                //DataInputStream din5 = new DataInputStream(this.nodes.get(i).getInputStream());
+                this.outputStreams.get(i).writeInt(2);
+                this.inputStreams.get(i).readInt();
+                int randomNum = this.randomNum(this.connections-1, i);
+                //DataOutputStream dout3 = new DataOutputStream(this.nodes.get(randomNum).getOutputStream());
+                //DataInputStream din8 = new DataInputStream(this.nodes.get(randomNum).getInputStream());
+                this.outputStreams.get(randomNum).writeInt(3);
+                //dout3.writeInt(3);
+                byte[] identifier = this.socketInfo.get(i)[0].getBytes();
+                int hostNameLength = identifier.length;
+                this.outputStreams.get(randomNum).writeInt(hostNameLength);
+                this.outputStreams.get(randomNum).write(identifier);
+                this.outputStreams.get(randomNum).writeInt(Integer.parseInt(this.socketInfo.get(i)[1]));
+                //dout3.writeInt(hostNameLength);
+                //dout3.write(identifier);
+                //dout3.writeInt(Integer.parseInt(this.socketInfo.get(i)[1]));
+                //din5.readInt();
+                this.inputStreams.get(i).readInt();
+                this.inputStreams.get(randomNum).readInt();
+                //din8.readInt();
+
             }
         }
+        for(int i = 0; i < this.connections; i++){
+            DataOutputStream dout4 = new DataOutputStream(this.nodes.get(i).getOutputStream());
+            DataInputStream din4 = new DataInputStream(this.nodes.get(i).getInputStream());
+            dout4.writeInt(1);
+            int sendTracker = din4.readInt();
+            int recieveTracker = din4.readInt();
+            Long recieveSummation2 = din4.readLong();
+            Long sendSummation2 = din4.readLong();
+            this.recieveSummation += recieveSummation2;
+            this.sendSummation += sendSummation2;
+            String output = this.socketInfo.get(i)[0] + " sent " + String.valueOf(sendTracker) + " for a total of " + String.valueOf(sendSummation) + " and recieved " + String.valueOf(recieveTracker) + " for a total of " + String.valueOf(recieveSummation);
+            System.out.println(output);
+            din4.readInt();
+
+        }
+        String output = "Sent- " + String.valueOf(this.sendSummation) + " Recieved- " + String.valueOf(this.recieveSummation);
+        System.out.println(output);
+        this.test();
+
+    }
+
+
+
+    public void test(){
+        try {
+            for(int i = 0; i < this.connections; i++){
+                DataOutputStream dout5 = new DataOutputStream(this.nodes.get(i).getOutputStream());
+                dout5.writeInt(0);
+
+            }
+
+        }
+        catch(IOException e){
+
+        }
+
     }
 
     public int randomNum(int max, int currentIndex){
@@ -105,16 +157,17 @@ public class Collator {
             while (myReader.hasNextLine()) {
                 String[] data = myReader.nextLine().split(",",2);
                 if(data[0].equals(name)){
-                    portNum = Integer.parseInt(data[1]);  
+                    portNum = Integer.parseInt(data[1]);
                 }
                 else{
                     ++totHosts;
                 }
-                
+
             }
             myReader.close();
             master = new Collator(name,portNum,totHosts);
             master.listenForRegistrations();
+            //master.test();
             master.startMessagePassing();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
@@ -126,6 +179,6 @@ public class Collator {
         }
 
     }
-       
+
 
 }
--- Assignment 1/src/main/java/cs455/overlay/Node.java
+++ Assignment 1/src/main/java/cs455/overlay/Node.java
@@ -34,17 +34,18 @@ public class Node implements Serializable{
 
     public void reciever(String serverAddress, int port) throws IOException {
         try {
-            this.socket = new Socket(serverAddress, port);
+            Socket socket = new Socket(serverAddress, port);
+            DataInputStream newDin = new DataInputStream(socket.getInputStream());
+            for (int i = 0; i < 5; i++) {
+                this.recieveTracker++;
+                int temp = newDin.readInt();
+                this.recieveSummation += temp;
+                //System.out.println("Recieved the number " + temp);
+            }
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        this.din = new DataInputStream(this.socket.getInputStream());
-        for (int i = 0; i < 5; i++) {
-            this.recieveTracker++;
-            int temp = din.readInt();
-            this.recieveSummation += temp;
-            System.out.println("Recieved the number " + temp);
-        }
+
     }
 
     public void closeReciever() throws IOException {
@@ -54,17 +55,21 @@ public class Node implements Serializable{
 
     public void sender() throws IOException {
         this.serverSocket = new ServerSocket(this.port, 5);
-        this.socket = this.serverSocket.accept();
-        this.dout = new DataOutputStream(this.socket.getOutputStream());
+        this.dout.writeInt(0);
+        Socket socket = this.serverSocket.accept();
+        //System.out.println("connection accepted");
+        DataOutputStream newdout = new DataOutputStream(socket.getOutputStream());
         Random rand = new Random();
         int randInt = 0;
         for (int i = 0; i < 5; i++) {
             randInt = rand.nextInt();
-            this.dout.writeInt(randInt);
+            newdout.writeInt(randInt);
             this.sendTracker++;
             this.sendSummation += randInt;
-            System.out.println("Sending the number " + randInt);
+            //System.out.println("Sending the number " + randInt);
         }
+        this.serverSocket.close();
+        newdout.close();
     }
 
     public void closeSender() throws IOException {
@@ -76,9 +81,48 @@ public class Node implements Serializable{
         try{
             System.out.println(InetAddress.getLocalHost().getHostName());
         }
-        
+
         catch (UnknownHostException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void messageType1(){
+        try{
+            this.dout.writeInt(this.sendTracker);
+            this.dout.writeInt(this.recieveTracker);
+            this.dout.writeLong(this.recieveSummation);
+            this.dout.writeLong(this.sendSummation);
+        }
+        catch(IOException e){
+        }
+    }
+
+    public void messageType2(){
+        try{
+            this.sender();
+        }
+        catch(IOException e){
+
+        }
+
+    }
+
+    public void messageType3(){
+        try{
+            //System.out.println("Here to set up connection");
+            int nameLength = this.din.readInt();
+            byte[] identifierBytes = new byte[nameLength];
+            this.din.readFully(identifierBytes);
+            String connectHostName = new String(identifierBytes);
+            connectHostName += ".cs.colostate.edu";
+            //System.out.println(connectHostName);
+            int connectPortNum = this.din.readInt();
+
+            this.reciever(connectHostName, connectPortNum);
+        }
+        catch(IOException e){
+
         }
     }
 
@@ -89,12 +133,34 @@ public class Node implements Serializable{
             e.printStackTrace();
         }
         this.dout = new DataOutputStream(this.socket.getOutputStream());
+        this.din = new DataInputStream(this.socket.getInputStream());
         byte[] identifier = this.hostName.getBytes();
         int hostNameLength = identifier.length;
         this.dout.writeInt(hostNameLength);
         this.dout.write(identifier);
+        this.dout.writeInt(this.port);
+        while(true){
+            int messageType = this.din.readInt();
+            //System.out.println(messageType);
+            if(messageType == 0){
+                break;
+            }
+            else if(messageType == 1){
+                this.messageType1();
+                this.dout.writeInt(0);
+            }
+            else if(messageType == 2){
+                this.messageType2();
+                this.dout.writeInt(0);
+            }
+            else if(messageType == 3){
+                this.messageType3();
+                this.dout.writeInt(0);
+            }
 
-       /* DataOutputStream serverOut = new DataOutputStream(s.getOutputStream()); 
+        }
+
+       /* DataOutputStream serverOut = new DataOutputStream(s.getOutputStream());
         DataInputStream serverIn = new DataInputStream(s.getInputStream());
         byte[] identifier = this.hostName.getBytes();
         int hostNameLength = identifier.length;

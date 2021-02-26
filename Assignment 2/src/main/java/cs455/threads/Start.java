5317d099a4cc6fe94ff49e2a551f08de7b259bad
Jean-Marc Ruffalo-Burgat <me@jeanmarc.us>
2021-02-26 12:40:38 -0700

    Start of Assignment 2

---
+++ Assignment 2/src/main/java/cs455/threads/Start.java
@@ -0,0 +1,11 @@
+package cs455.threads;
+
+public class Start{
+
+    public static void main(String[] args){
+        for(int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+
+}
